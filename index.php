26e658ec380a022035060d050b6503996c801e26
Jhonatan Santos <130712504+Jhonysc43@users.noreply.github.com>
2025-03-04 17:37:24 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chamativos Bar</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body style="background-image: url('head.png');">
+    <header>
+        <div class="header-content" id="home">
+            <img src="img/logo.png" alt="Logo do Chamativos Bar" class="logo" style="width:100px">
+        <h1>Bem-vindo ao Chamativos Bar!</h1>
+        </div>
+        <p id="welcomeMessage"></p>
+    </header>
+    <nav class="navbar" style="background-color: #333; padding: 10px; width: 100%;">
+            <ul class="nav-list" style="list-style-type: none; margin: 0; padding: 0; display: flex; justify-content: center;">
+                <li class="nav-item" style="margin: 0 15px;">
+                    <a href="#home" style="color: white">Home</a>
+                </li>
+                <li class="nav-item" style="margin: 0 15px;">
+                    <a href="#cardapio" style="color: white">Cardápio</a>
+                </li> 
+                <li class="nav-item" style="margin: 0 15px;">
+                    <a href="#tiragosto" style="color: white">Tira Gosto</a>
+                </li> 
+                <li class="nav-item" style="margin: 0 15px;">
+                    <a href="#bebidas" style="color: white">Bebidas</a>
+                </li> 
+            </ul> 
+        </nav>
+
+    <main>
+        <section class="about">
+            <h2>Sobre Nós</h2>
+            <p>No Chamativos Bar, celebramos a amizade e a simplicidade em um ambiente familiar, onde cada visita se torna uma lembrança inesquecível.</p>
+            <p>Nosso espaço é perfeito para relaxar após um dia longo, encontrar amigos e desfrutar de uma boa conversa. Oferecemos uma variedade de bebidas refrescantes e deliciosos tira-gostos.</p>
+            <p>Seja para uma noite animada ou uma reunião tranquila, o Chamativos Bar é o seu ponto de encontro ideal. Venha fazer parte da nossa família e sinta-se em casa!</p>
+        </section>
+        <section  class="cardapio" id="cardapio">
+            <div class="services-content container" data-aos="zoom-in-up" data-aos-delay="200">
+             <h2>Cardápio</h2>
+        
+            </div>
+            <section class="haircuts">
+             <div class="haircut" data-aos="fade-up" data-aos-delay="400">
+                 <img src="img/foto3.png" alt="Feijoada"/>
+                 <div class="haircut-info">
+                     <strong>Sabado: Feijoada</strong>
+                     <button>
+                         R$ 15,00
+                     </button>
+                 </div>
+                 
+             </div>
+             <div class="haircut" data-aos="fade-up" data-aos-delay="600">
+                 <img
+                 src="img/foto4.png"
+                 alt="dobradinha com mocotó"
+                 />
+                 <div class="haircut-info">
+                     <strong>Domingo: Dobradinha</strong>
+                     <button>
+                         R$ 15,00
+                     </button>
+                 </div>
+             </div>
+            </section>
+         </section>
+
+         <section  class="cardapio" id="tiragosto">
+            <div class="services-content container" data-aos="zoom-in-up" data-aos-delay="200">
+             <h2>O melhor tira gosto você encontra aqui</h2>
+        
+            </div>
+            <section class="haircuts">
+                <div class="haircut" data-aos="fade-up" data-aos-delay="200">
+                    <img src="img/pastel.png" alt="pastel"/>
+                    <div class="haircut-info">
+                        <strong>Pastel: </strong>
+                        <button>
+                            R$ 4,00
+                        </button>
+                    </div>
+                </div>
+                <div class="haircut" data-aos="fade-up" data-aos-delay="200">
+                 <img src="img/coxinha.png" alt="coxinha"/>
+                 <div class="haircut-info">
+                     <strong>Coxinha:  </strong>
+                     <button>
+                         R$ 4,00
+                     </button>
+                 </div>
+             </div>
+            </section>
+         </section>
+
+         <section  class="cardapio" id="bebidas">
+            <div class="services-content container" data-aos="zoom-in-up" data-aos-delay="200">
+             <h2>Bebidas</h2>
+        
+            </div>
+            <section class="haircuts">
+             <div class="haircut" data-aos="fade-up" data-aos-delay="400">
+                 <img src="img/brahma.jpg" alt="brahma"/>
+                 <div class="haircut-info">
+                     <strong>Brahma Chopp<br>600ml: </strong>
+                     <button>
+                         R$ 8,50
+                     </button>
+                 </div>
+                 
+             </div>
+             <div class="haircut" data-aos="fade-up" data-aos-delay="600">
+                 <img
+                 src="img/antarctica.jpg"
+                 alt="antarctica"
+                 />
+                 <div class="haircut-info">
+                     <strong>Antarctica<br>600ml: </strong>
+                     <button>
+                         R$ 8,50
+                     </button>
+                 </div>
+             </div>
+             <div class="haircut" data-aos="fade-up" data-aos-delay="600">
+                 <img
+                 src="img/itaipava.jpg"
+                 alt="antarctica"
+                 />
+                 <div class="haircut-info">
+                     <strong>Itaipava<br>1 Litro: </strong>
+                     <button>
+                         R$ 9,00
+                     </button>
+                 </div>
+             </div>
+            </section>
+         </section>
+
+                  <section  class="cardapio">
+            <div class="services-content container" data-aos="zoom-in-up" data-aos-delay="200">
+            </div>
+            <section class="haircuts">
+             <div class="haircut" data-aos="fade-up" data-aos-delay="400">
+                 <img src="img/brahma269.jpg" alt="brahma"/>
+                 <div class="haircut-info">
+                     <strong>Brahma lata<br>269ml: </strong>
+                     <button>
+                         R$ 4,00
+                     </button>
+                 </div>
+                 
+             </div>
+             <div class="haircut" data-aos="fade-up" data-aos-delay="600">
+                 <img
+                 src="img/antarctica269.jpg"
+                 alt="antarctica"
+                 />
+                 <div class="haircut-info">
+                     <strong>Antarctica lata<br>269ml: </strong>
+                     <button>
+                         R$ 4,00
+                     </button>
+                 </div>
+             </div>
+             <div class="haircut" data-aos="fade-up" data-aos-delay="600">
+                 <img
+                 src="img/skol269ml.jpg"
+                 alt="antarctica"
+                 />
+                 <div class="haircut-info">
+                     <strong>Skol lata<br> 269ml: </strong>
+                     <button>
+                         R$ 4,00
+                     </button>
+                 </div>
+             </div>
+            </section>
+        </section>
+
+
+
+        <section class="hours">
+            <h2>Horário de Funcionamento</h2>
+            <p>Segunda a quinta: 10h - 22h</p>
+            <p>Sexta a Domingo: 10h - 00h</p>
+        </section>
+        <section class="hours">
+            <h2>Venha nos visitar</h2>
+        </section>
+        <iframe
+        width="100%"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d240.33649032682968!2d-47.61615157441416!3d-15.463771946878563!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935a6f5a91ffb535%3A0x7118653275b5dd30!2sChamativos%20Bar!5e0!3m2!1spt-BR!2sbr!4v1694374504944!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            
+            <a
+            href="https://wa.me//5561981429502?text=Quero%20fazer%20um%20agendamento"
+            class="btn-whatsapp"
+            target="_blank"
+            data-aos="zoom-in-up" data-aos-delay="400"
+            >
+            <img src="img/whatssapp.svg" alt="whatssapp"/>
+            <span class="tooltip-text">Entre em contato</span>
+        
+        </a>
+    </main>
+    <footer class="footer">
+        <div>
+        <img src="img/logo.png" class="logo" alt="logo Chamativos Bar" style="width:100px" />
+        </div>
+        <p>Copyright 2024 | JS.Web - Todos os direitos reservados</p>
+    </footer>
+
+    <div class="navbar" style="background-color: #333; padding: 10px; width: 100%;">
+            <ul class="nav-list" style="list-style-type: none; margin: 0; padding: 0; display: flex; justify-content: center;">
+                <li class="nav-item" style="margin: 0 15px;">
+                    <a href="#home" style="color: white">Voltar para o início</a>
+                </li>
+            </ul>
+        </div>
+
+    <script src="js/script.js"></script>
+</body>
+</html>
